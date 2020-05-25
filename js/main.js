ffe6644cb8bb0e78b423a047bbfa6b1744dc9ce6
DacD <dacdao01@gmail.com>
2020-05-24 23:33:35 -0400

    fix subscription (sendinblue) and fix mobile menu

--- js/main.js
+++ js/main.js
@@ -81,10 +81,12 @@
     // Slick Nav
     $('.mobile-menu').slicknav({
       prependTo: '.navbar-header',
+      closeOnClick: true,
       parentTag: 'span',
       allowParentLinks: true,
       duplicate: false,
       label: '',
+
     });
 
 
@@ -204,61 +206,5 @@
   $('#loader').fadeOut();
 
 
-// for contact us section of the page
-// $("#form").validator().on("submit", function (event) {
-// if (event.isDefaultPrevented()) {
-//  // handle the invalid form...
-//  formError();
-//  submitMSG(false, "Did you fill in the form properly?");
-// } else {
-//  // everything looks good!
-//  event.preventDefault();
-//  submitForm();
-//   }
-// });
-//
-//
-//   function submitForm(){
-//       // Initiate Variables With Form Content
-//       var name = $("#name").val();
-//       var email = $("#email").val();
-//       var message = $("#message").val();
-//
-//
-//       $.ajax({
-//           type: "POST",
-//           url: "php/form-process.php",
-//           data: "name=" + name + "&email=" + email + "&message=" + message,
-//           success : function(text){
-//               if (text == "success"){
-//                   formSuccess();
-//               } else {
-//                   formError();
-//                   submitMSG(false,text);
-//               }
-//           }
-//       });
-//   }
-//
-//   function formSuccess(){
-//       $("#form")[0].reset();
-//       submitMSG(true, "Message Submitted!")
-//   }
-//
-//   function formError(){
-//       $("#form").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-//           $(this).removeClass();
-//       });
-//   }
-//
-//   function submitMSG(valid, msg){
-//       if(valid){
-//           var msgClasses = "h3 text-center tada animated text-success";
-//       } else {
-//           var msgClasses = "h3 text-center text-danger";
-//       }
-//       $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-//   }
-//
 
 }(jQuery));
